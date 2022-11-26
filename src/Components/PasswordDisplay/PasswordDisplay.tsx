ff43ea301672c5ac5cb2474b1991be6365cc69df
Emil <emiliano.milewski@gmail.com>
2022-11-26 15:30:10 +0000

    added PasswordDisplay jsx && css

--- src/Components/PasswordDisplay/PasswordDisplay.tsx
+++ src/Components/PasswordDisplay/PasswordDisplay.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import "./PasswordDisplay.css";
+import copy from "../../Imgs/copy.svg";
 
-function PasswordDisplay() {
+function PasswordDisplay({ password }: { password: string }) {
+    function copyClipboard(text: string) {
+        navigator.clipboard.writeText(text);
+    }
+
     return (
         <div className="password-display">
-
+            <p className="password-display__text">{password}</p>
+            <button className="password-display__copy-btn" onClick={() => copyClipboard(password)}><img className="password-display__copy-img" src={copy} /></button>
         </div>
     )
 }
 
-export default PasswordDisplay();
+export default PasswordDisplay;
