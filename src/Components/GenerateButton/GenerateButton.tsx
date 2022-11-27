3c5101dbfce2958076f8c5d48880cae04a3eff1a
Emil <emiliano.milewski@gmail.com>
2022-11-27 20:07:59 +0000

    wrote basic css&&html for GenerateButton

--- src/Components/GenerateButton/GenerateButton.tsx
+++ src/Components/GenerateButton/GenerateButton.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./GenerateButton.css";
+import rightArrow from "../../Imgs/right-arrow.svg";
 
 function GenerateButton() {
     return (
         <div className="generate-button">
-
+            <button className="generate-button__btn">
+                <span className="generate-button__btn-text">Generate</span>
+                <img className="generate-button__btn-img" src={rightArrow} alt="right arrow" />
+            </button>
         </div>
     )
 }
