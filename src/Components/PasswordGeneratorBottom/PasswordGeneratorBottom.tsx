9efa38f569f3b23965e3736abcc4827ceb990eb2
Emil <emiliano.milewski@gmail.com>
2022-11-30 19:08:17 +0000

    passed down props

--- src/Components/PasswordGeneratorBottom/PasswordGeneratorBottom.tsx
+++ src/Components/PasswordGeneratorBottom/PasswordGeneratorBottom.tsx
@@ -3,15 +3,22 @@ import "./PasswordGeneratorBottom.css";
 import GeneratorOptions from "../GeneratorOptions/GeneratorOptions";
 import StrengthMeter from "../StrengthMeter/StrengthMeter";
 import GenerateButton from "../GenerateButton/GenerateButton";
+import Options from "../../Interfaces/Options";
 
-function PasswordGeneratorBottom() {
+interface PasswordGeneratorBottomProps {
+    generatePassword: Function,
+    options: Options,
+    updateOptions: Function,
+}
+
+function PasswordGeneratorBottom({ generatePassword, options, updateOptions }: PasswordGeneratorBottomProps) {
     return (
         <div className="password-generator-bottom">
-            <GeneratorOptions />
+            <GeneratorOptions updateOptions={updateOptions} />
             <StrengthMeter strengthText="placeholder" />
-            <GenerateButton />
+            <GenerateButton options={options} generatePassword={generatePassword} />
         </div>
     );
-}
+};
 
 export default PasswordGeneratorBottom;
