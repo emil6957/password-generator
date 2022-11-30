14488668dd0057c9cc98b9e08288c588940360b3
Emil <emiliano.milewski@gmail.com>
2022-11-30 19:08:08 +0000

    Added new states and functions

--- src/Components/PasswordGenerator/PasswordGenerator.tsx
+++ src/Components/PasswordGenerator/PasswordGenerator.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PasswordGenerator.css"
+import Options from "../../Interfaces/Options";
 import PasswordDisplay from "../PasswordDisplay/PasswordDisplay";
 import PasswordGeneratorBottom from "../PasswordGeneratorBottom/PasswordGeneratorBottom";
 
 function PasswordGenerator() {
+    const [password, setPassword] = useState<string>("");
+    const [options, setOptions] = useState<Options>({
+        uppercase: false,
+        lowercase: false,
+        numbers: false,
+        symbols: false,
+    });
+
+    function generatePassword(): void {
+    };
+
+    function updateOptions(option: string): void {
+    };
+
     return (
         <div className="password-generator">
             <h1 className="password-generator__header">Password Generator</h1>
-            <PasswordDisplay password={"placeholder"} />
-            <PasswordGeneratorBottom />
+            <PasswordDisplay password={password} />
+            <PasswordGeneratorBottom options={options} updateOptions={updateOptions} generatePassword={generatePassword} />
         </div>
-    )
+    );
 };
 
 export default PasswordGenerator;
