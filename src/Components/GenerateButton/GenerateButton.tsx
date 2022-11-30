96598fb6bed336f34849e8f7a9fa1b28cc8bf706
Emil <emiliano.milewski@gmail.com>
2022-11-30 19:07:17 +0000

    Added interfaces to declare prop types + passed down function

--- src/Components/GenerateButton/GenerateButton.tsx
+++ src/Components/GenerateButton/GenerateButton.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./GenerateButton.css";
 import rightArrow from "../../Imgs/right-arrow.svg";
+import Options from "../../Interfaces/Options";
 
-function GenerateButton() {
+interface GenerateButtonProps {
+    generatePassword: Function,
+    options: Options,
+};
+
+function GenerateButton({ generatePassword, options }: GenerateButtonProps) {
     return (
         <div className="generate-button">
-            <button className="generate-button__btn">
+            <button className="generate-button__btn" onClick={() => generatePassword()}>
                 <span className="generate-button__btn-text">Generate</span>
                 <img className="generate-button__btn-img" src={rightArrow} alt="right arrow" />
             </button>
         </div>
-    )
-}
+    );
+};
 
 export default GenerateButton;
