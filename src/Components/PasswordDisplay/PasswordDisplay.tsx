e4ae2b905ce164028ddc6da1632b2f154b649db5
Emil <emiliano.milewski@gmail.com>
2022-11-30 19:07:35 +0000

    Added interfaces to declare prop types + added placeholder text

--- src/Components/PasswordDisplay/PasswordDisplay.tsx
+++ src/Components/PasswordDisplay/PasswordDisplay.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import "./PasswordDisplay.css";
 import copy from "../../Imgs/copy.svg";
 
-function PasswordDisplay({ password }: { password: string }) {
+interface PasswordDisplayProps {
+    password: string,
+};
+
+function PasswordDisplay({ password }: PasswordDisplayProps) {
     function copyClipboard(text: string) {
         navigator.clipboard.writeText(text);
-    }
+    };
 
     return (
         <div className="password-display">
-            <input className="password-display__text" type="text" value={password} disabled />
+            <input className="password-display__text" type="text" value={password} placeholder="Password123!" disabled />
             <button className="password-display__copy-btn" onClick={() => copyClipboard(password)}><img className="password-display__copy-img" src={copy} /></button>
         </div>
-    )
-}
+    );
+};
 
 export default PasswordDisplay;
